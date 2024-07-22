65ec0269546d49919b8fca16bcaf2188eb37d002
RohanSinghTakhi <co23545@ccet.ac.in>
2024-07-22 21:23:44 +0530

    about done

--- src/pages/NOPage.jsx
+++ src/pages/NOPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Layout from '../components/layouts/Layout';
+import { Center } from '@chakra-ui/react';
 
 function NOPage() {
   return (
-    <div>NOPage</div>
-  )
+    <Layout>
+      <Center style={{ height: "90vh", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <h1 className='text-[100px] font-medium'>404 Not Found</h1>
+        <p className='text-[20px] mt-3 mb-24'>Your visited page not found. You may go home page.</p>
+        <Link to="/">
+          <button type="button" className="btn btn-danger w-56 h-12">Back to home page</button>
+        </Link>
+      </Center>
+    </Layout>
+  );
 }
 
-export default NOPage
+export default NOPage;
