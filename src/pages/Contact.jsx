57afc44c451095da2ed920f2d928a9e26540b154
RohanSinghTakhi <co23545@ccet.ac.in>
2024-07-23 20:49:24 +0530

    contact completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,30 +1,85 @@
 import React from 'react'
 import Layout from '../components/layouts/Layout'
 import ima from "../assets/phone_about.png"
+import mail from"../assets/mail_about.png"
+
+
 
 function Contact() {
+
+  const box = [{ text: " Your Name" }, { text: " Your Email" }, { text: " Your Phone" }]
   return (
     <Layout>
-
-<body style={{
-            height:"90vh",display:"flex",justifyContent:"center",alignItems:"center"
-        }}>
-            
-        <div style={{backgroundColor:"blue",height:"80vh",width:"80%", display:"flex",justifyContent:"space-between" }}>
-           <div className="about_box_1" style={{width:"40%",height:"100%" ,backgroundColor:"#f5f2f2" ,display:"flex",flexDirection:"column",alignContent:"center",alignItems:"center"}}>
-            <div className="1"style={{width:"90%",height:"50%"  ,borderBottom:"1px solid grey"}}>
-                <div className='flex items-center '> 
-                    <img style={{height:"40px",marginRight:"5px",}} src={ima} alt="" srcset="" />
-                    <h1 className='text-[20px] font-medium'>Call To us </h1>
+      <section className='about_box mb-28 ' style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "98vw" }} >
+        <div style={{ height: "70vh", width: "90vw", display: "flex", justifyContent: "space-evenly", marginTop: "80px" }}>
+          <div style={{ width: "30%",  boxShadow: "1px 1px 5px grey", display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center" }}>
+                <div style={{width:"80%",height:"35%" ,borderBottom:"2px solid #D9D9D9"}}>
+                  <div style={{display:"flex" ,alignItems:"center"}}> 
+                    <img width="50px" src={ima} alt="" />
+                    <h1 className='text-[25px] ml-4 font-medium'>Call To Us </h1>
+                  </div>
+                    <p className='text-[17px] mt-4 font-medium'>We are available 24/7, 7 days a week.</p>
+                    <p className='text-[17px] mt-4 font-medium'>Phone: +91 9999988888</p>
                 </div>
-            </div>
-            <div className="1"style={{width:"90%",height:"50%" }}>
+                <div style={{width:"80%",height:"45%" }}>
+                <div style={{display:"flex" ,alignItems:"center" ,marginTop:"30px"}}> 
+                    <img width="50px"  src={mail} alt="" />
+                    <h1 className='text-[25px] ml-4 font-medium'>Call To Us </h1>
+                  </div>
+                    <p className='text-[17px] mt-5 font-medium'>Fill out our form and we will contact you within 24 hours.</p>
+                    <p className='text-[17px] mt-3 font-medium'>Emails: customer@exclusive.com</p>
+                    <p className='text-[17px] mt-3 font-medium'>Emails: support@exclusive.com</p>
+                </div>
+          </div>
+          <div style={{ position: 'relative',width: "65%", backgroundColor: "white", boxShadow: "1px 2px 5px grey", display: 'flex', flexDirection: "column", alignItems: "center" }}>
+            <div style={{ width: "90%", height: "50px", marginTop: "50px", display: "flex", justifyContent: "space-between" }}>
+              {box.map((item, index) => (
+                <input
+                  key={index}
+                  style={{
+                    backgroundColor: "#F5F5F5",
+                    width: "32%",
+                    height: "auto",
+                    border: "none",
+                    outline: "none",
+                    padding: "8px",
+                    color: "grey",
+                    fontSize: "17px"
+
+                  }}
+                  type="text"
+                  placeholder={item.text}
+                />
+              ))}
+
 
             </div>
-           </div>
-           <div className="about_box_2 " style={{width:"55%" ,height:"100%",backgroundColor:"green"}}></div>
+            <textarea 
+  style={{ 
+    width: "90%", 
+    height: "50%", 
+    marginTop: "30px", 
+    backgroundColor: "#F5F5F5", 
+    border: "none", 
+    outline: "none", 
+    padding: "10px"
+  }}
+  placeholder="Your Massage"
+/>
+
+<button  type="button" style={{ position: 'absolute', right: '40px',bottom:"34px" }} className="btn btn-danger text-[16px] w-52 h-14 mt-8 ">Send Message</button>
+
+
+          </div>
+          
+
+
         </div>
-        </body>
+
+
+      </section>
+      
+
     </Layout>
   )
 }
